cf0c3d49f8019a5a332df76b16c516486a35bffa
Adam McDaniel <adam@array.org>
2019-09-01 19:48:48 -0600

    Initial commit

---
+++ arduino-clockradio.ino
@@ -0,0 +1,18 @@
+#include <ESP8266WiFi.h>
+#include <WiFiUdp.h>
+#include "ht16k33.h"
+#include "asciifont.h"
+#include "secret.h"
+
+
+void setup() {
+    // Initialize Wifi
+
+    // Get time from NTP
+}
+
+void loop() {
+    // Update Clock Display
+
+    delay(1000);
+}
