3d5aae3a2e853d388f1530c705cac6eee83af7e6
Adam McDaniel <adam@array.org>
2021-03-15 17:16:01 -0600

    Logic reorg to utilize ESP Deep sleep during 60 second refreshes
    Stores the time in a pseudo-RTC memory block.  Also refreshes NTP after X
    minutes to take into account drift.

--- esp8266-clockradio.ino
+++ esp8266-clockradio.ino
@@ -53,21 +53,32 @@ IPAddress   ntpServerIP;
 
 const int NTP_PACKET_SIZE = 48; // NTP time stamp is in the first 48 bytes of the message
 byte packetBuffer[NTP_PACKET_SIZE]; //buffer to hold incoming and outgoing packets
-unsigned long localSeconds = 0;
+uint32_t localSeconds = 0;
 
-const long dstEnabledOnEpoch  = 1615716000; // March 14 2021 10am UTC
-const long dstEnabledSeconds  = 10886400;   // DST Enabled for 5 months 7 days
-const long dstDisabledSeconds = 20563200;   // DST Disabled for 8 months 27 days
+uint8_t hours = 0;
+uint8_t minutes = 0;
+uint8_t seconds = 0;
+
+const uint32_t dstEnabledOnEpoch  = 1615716000; // March 14 2021 10am UTC
+const uint32_t dstEnabledSeconds  = 10886400;   // DST Enabled for 5 months 7 days
+const uint32_t dstDisabledSeconds = 20563200;   // DST Disabled for 8 months 27 days
 
 // Mountain Standard Time and Mountain Daylight Time offsets from GMT by seconds.
-const long  mstOffset_sec = -7 * 3600;
-const long  mdtOffset_sec = -6 * 3600;
+const uint32_t mstOffset_sec = -7 * 3600;
+const uint32_t mdtOffset_sec = -6 * 3600;
+
+struct {
+  uint32_t crc32;
+  uint32_t epoch;
+  uint32_t refresh;
+} rtcData;
 
 WiFiUDP udp;
 HT16K33 ht;
 
 // An arbitrary function at NULL pointer forces the hardware to be reset.
 void (*reset)(void) = 0;
+
 
 void setup() {
     Serial.begin(115200);
@@ -76,12 +87,53 @@ void setup() {
     ht.define16segFont((uint16_t *)&fontTable);
     ht.setBrightness(7);
 
-    ht.set16Seg(0, 'N');
-    ht.set16Seg(1, 'T');
-    ht.set16Seg(2, 'P');
-    ht.set16Seg(3, '*');
-    ht.sendLed();
+    uint8_t goodRTC = loadMemory();
+    uint8_t goodNTP = 0;
 
+    if (goodRTC) {
+        // If our memory is good, and we're under the refresh NTP time,
+        // exit setup right away and jump to the loop()
+        Serial.print("rtcData.epoch = ");
+        Serial.println(rtcData.epoch);
+        Serial.print("rtcData.refresh = ");
+        Serial.println(rtcData.refresh);
+
+        localSeconds = rtcData.epoch;
+        if (rtcData.epoch < rtcData.refresh)
+            goodNTP = 1;
+    }
+
+    if (!goodRTC) {
+        ht.set16Seg(0, 'N');
+        ht.set16Seg(1, 'T');
+        ht.set16Seg(2, 'P');
+        ht.set16Seg(3, '*');
+        ht.sendLed();
+    }
+
+    if (!goodNTP) {
+        getTimeFromNTP();  // sets localSeconds
+    }
+}
+
+
+void loop() {
+    hours = (localSeconds % 86400L)/ 3600;
+    minutes = (localSeconds % 3600) / 60;
+    seconds = localSeconds % 60;
+
+    updateDisplay();
+
+    // At SLEEP_TIME, or anytime after, shutdown the display.
+    if ((hours == SLEEP_TIME_HOURS && minutes >= SLEEP_TIME_MINUTES) ||
+         hours  > SLEEP_TIME_HOURS)
+        longSleep();
+    else
+        shortSleep();
+}
+
+
+uint32_t getTimeFromNTP(){
     // Initialize Wifi
 
     Serial.print("Connecting to ");
@@ -121,6 +173,7 @@ void setup() {
         ht.set16Seg(2, 'r');
         ht.set16Seg(3, 'y');
         ht.sendLed();
+        clearMemory();
         reset();
     } else {
         Serial.print("packet received, length=");
@@ -131,17 +184,17 @@ void setup() {
         //the timestamp starts at byte 40 of the received packet and is four bytes,
         // or two words, long. First, esxtract the two words:
 
-        unsigned long highWord = word(packetBuffer[40], packetBuffer[41]);
-        unsigned long lowWord = word(packetBuffer[42], packetBuffer[43]);
+        uint32_t highWord = word(packetBuffer[40], packetBuffer[41]);
+        uint32_t lowWord = word(packetBuffer[42], packetBuffer[43]);
         // combine the four bytes (two words) into a long integer
         // this is NTP time (seconds since Jan 1 1900):
-        unsigned long secsSince1900 = highWord << 16 | lowWord;
+        uint32_t secsSince1900 = highWord << 16 | lowWord;
         Serial.print("Seconds since Jan 1 1900 UTC = ");
         Serial.println(secsSince1900);
 
         // now convert NTP time into everyday time:
         // Unix time starts on Jan 1 1970. In seconds, that's 2208988800:
-        const unsigned long seventyYears = 2208988800UL;
+        const uint32_t seventyYears = 2208988800UL;
         // subtract seventy years:
         time_t epoch = secsSince1900 - seventyYears;
 
@@ -177,6 +230,9 @@ void setup() {
         Serial.println(localSeconds);
 
         localSeconds += 2;  // extra two seconds to offset the 2000ms delay accessing NTP
+
+        rtcData.epoch = localSeconds;
+        rtcData.refresh = localSeconds + 3600; // Force an NTP refresh in an hour
     }
 
 
@@ -192,13 +248,9 @@ void setup() {
     delay(1);
 }
 
-void loop() {
+
+void updateDisplay() {
     // Update Clock Display
-
-    uint8_t hours = (localSeconds % 86400L)/ 3600;
-    uint8_t minutes = (localSeconds % 3600) / 60;
-    uint8_t seconds = localSeconds % 60;
-
     const char* digits = "0123456789";
 
     // print the hour, minute and second:
@@ -210,7 +262,7 @@ void loop() {
     Serial.println(seconds);
 
     // Flash at 5pm... it's time to go home!
-    if (hours == 17)
+    if (hours == SLEEP_TIME_HOURS)
         ht.setBlinkRate(HT16K33_DSP_BLINK1HZ);
 
     if (hours >= 10)
@@ -230,45 +282,52 @@ void loop() {
     ht.set16Seg(3, digits[minutes % 10]);
 
     ht.sendLed();
+}
 
+
+void shortSleep() {
     uint8_t delaySeconds = 60 - seconds;
+
     Serial.print("Sleeping for ");
     Serial.print(delaySeconds);
     Serial.println(" seconds.");
 
-    localSeconds += delaySeconds;
-#ifdef DEBUG
-    while (delaySeconds-- >= 1) {
-        Serial.print("Waiting: ");
-        Serial.println(delaySeconds);
-        delay(1000);
-    }
-#else
-    delay(delaySeconds * 1000);
-#endif
+    rtcData.epoch += delaySeconds;
+    saveMemory();
 
-    // At SLEEP_TIME, or anytime after, shutdown the display.
-    if ((hours == SLEEP_TIME_HOURS && minutes >= SLEEP_TIME_MINUTES) ||
-         hours  > SLEEP_TIME_HOURS) {
-        int wakeupSeconds = 0;
-
-        // Calculate seconds until 9am tomorrow.
-        wakeupSeconds =  (24 - hours) * 3600;
-        wakeupSeconds += (60 - minutes) * 60;
-        wakeupSeconds += WAKEUP_TIME_HOURS * 3600;
-        wakeupSeconds += WAKEUP_TIME_MINUTES * 60;
-
-        Serial.println("Display off");
-        ht.displayOff();
-        Serial.println("Display Sleep");
-        ht.sleep();
-        // Wake up again at 9am tomorrow morning
-        Serial.print("Deep sleep for ");
-        Serial.print(wakeupSeconds);
-        Serial.println(" seconds");
-        ESP.deepSleep(wakeupSeconds * 1e6);
-    }
+    Serial.print("Deep sleep with screen on for ");
+    Serial.print(delaySeconds);
+    Serial.println(" seconds");
+    ESP.deepSleep(delaySeconds * 1e6);
 }
+
+
+void longSleep() {
+    int wakeupSeconds = 0;
+
+    // Calculate seconds until 9am tomorrow.
+    wakeupSeconds =  (24 - hours) * 3600;
+    wakeupSeconds += (60 - minutes) * 60;
+    wakeupSeconds += (60 - seconds);
+    wakeupSeconds += WAKEUP_TIME_HOURS * 3600;
+    wakeupSeconds += WAKEUP_TIME_MINUTES * 60;
+
+    rtcData.epoch += wakeupSeconds;
+
+    Serial.println("Display off");
+    ht.displayOff();
+    Serial.println("Display Sleep");
+    ht.sleep();
+    // Wake up again at 9am tomorrow morning
+    Serial.print("Deep sleep for ");
+    Serial.print(wakeupSeconds);
+    Serial.println(" seconds");
+
+    clearMemory(); // to force an NTP reset
+
+    ESP.deepSleep(wakeupSeconds * 1e6);
+}
+
 
 // send an NTP request to the time server at the given address
 void sendNTPpacket(IPAddress& address) {
@@ -292,4 +351,93 @@ void sendNTPpacket(IPAddress& address) {
     udp.beginPacket(address, 123); //NTP requests are to port 123
     udp.write(packetBuffer, NTP_PACKET_SIZE);
     udp.endPacket();
+}
+
+
+uint32_t calculateCRC32(const uint8_t *data, size_t length) {
+    uint32_t crc = 0xffffffff;
+
+    while (length--) {
+        uint8_t c = *data++;
+        for (uint32_t i = 0x80; i > 0; i >>= 1) {
+            bool bit = crc & 0x80000000;
+            if (c & i) {
+                bit = !bit;
+            }
+            crc <<= 1;
+            if (bit) {
+                crc ^= 0x04c11db7;
+            }
+        }
+    }
+    return crc;
+}
+
+
+void saveMemory() {
+    // Generate new data set for the struct
+    memset(&rtcData, sizeof(rtcData), '\0');
+
+    Serial.print("rtcData.epoch = ");
+    Serial.println(rtcData.epoch);
+    Serial.print("rtcData.refresh = ");
+    Serial.println(rtcData.refresh);
+
+    // Update CRC32 of data
+    rtcData.crc32 = calculateCRC32((uint8_t*) &rtcData.epoch, sizeof(rtcData)-sizeof(rtcData.crc32));
+    // Write struct to RTC memory
+    if (ESP.rtcUserMemoryWrite(0, (uint32_t*) &rtcData, sizeof(rtcData))) {
+        Serial.println("Write: ");
+        printMemory();
+        Serial.println();
+    }
+}
+
+
+uint8_t loadMemory() {
+    // Read struct from RTC memory
+    if (ESP.rtcUserMemoryRead(0, (uint32_t*) &rtcData, sizeof(rtcData))) {
+        Serial.println("Read: ");
+        printMemory();
+        Serial.println();
+        uint32_t crcOfData = calculateCRC32((uint8_t*) &rtcData.epoch, sizeof(rtcData)-sizeof(rtcData.crc32));
+        Serial.print("CRC32 of data: ");
+        Serial.println(crcOfData, HEX);
+        Serial.print("CRC32 read from RTC: ");
+        Serial.println(rtcData.crc32, HEX);
+        if (crcOfData != rtcData.crc32) {
+            Serial.println("CRC32 in RTC memory doesn't match CRC32 of data. Data is probably invalid!");
+        } else {
+            Serial.println("CRC32 check ok, data is probably valid.");
+        }
+        return (crcOfData == rtcData.crc32);
+    }
+    return 0;
+}
+
+
+void clearMemory() {
+    Serial.println("Clear RTC memory");
+
+    memset(&rtcData, sizeof(rtcData), '\0');
+
+    ESP.rtcUserMemoryWrite(0, (uint32_t*) &rtcData, sizeof(rtcData));
+}
+
+
+//prints all rtcData, including the leading crc32
+void printMemory() {
+    char buf[3];
+    uint8_t *ptr = (uint8_t *)&rtcData;
+
+    for (size_t i = 0; i < sizeof(rtcData); i++) {
+        sprintf(buf, "%02X", ptr[i]);
+        Serial.print(buf);
+        if ((i + 1) % 32 == 0) {
+            Serial.println();
+        } else {
+            Serial.print(" ");
+        }
+    }
+    Serial.println();
 }
